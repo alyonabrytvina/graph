d88974634cb44a3ea149704c924defe076d7586b
Alyona <alyona.ivanova.kh@gmail.com>
2022-12-19 14:31:18 +0100

    map transformation array, render basic structure, add styles - App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,40 @@
-export const App = () => {
+import React from "react";
+import "./App.scss";
+import { Source } from "../Source/Source";
+import { Action } from "../Action/Action";
+import { transformations } from "../../api/transformations";
+import { Target } from "../Target/Target";
+import { ActionRoot } from "../../interfaces/interfaces";
 
-};
+export const App = () => (
+  <>
+    {transformations.map(({ sources, actions, target }, index: number) => {
+      const { id: targetId } = target;
+
+      return (
+        <div className="graph">
+          <div className="graph__sources">
+            {sources.map(({ id }: Record<string, string>) => (
+              <Source
+                key={id}
+                sourceName={id}
+              />
+            ))}
+          </div>
+          <div className="graph__actions">
+            {actions.map((props: ActionRoot) => (
+              <Action
+                key={props.id}
+                props={props}
+                actionIndex={index}
+              />
+            ))}
+          </div>
+          <div className="graph__targets">
+            <Target targetName={targetId} />
+          </div>
+        </div>
+      );
+    })}
+  </>
+);
