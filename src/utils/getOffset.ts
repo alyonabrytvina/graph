bb2113e6604f73b01e9e25a24c969d3a5601976d
Alyona <alyona.ivanova.kh@gmail.com>
2022-12-19 14:26:45 +0100

    modified getOffset function, fixed checking for drawing lines

--- src/utils/getOffset.ts
+++ src/utils/getOffset.ts
@@ -1,40 +1,37 @@
-export const getOffset = (element) => {
+export const getOffset = (element: HTMLElement) => {
   const {
-    top, left, width, height, offsetHeight, offsetWidth, right,
-  } = element.getBoundingClientRect();
+    top, left, width, height, right,
+  }: DOMRect = element.getBoundingClientRect();
 
   return {
     top,
     left: left - width,
     right: right - width,
-    width: width || offsetHeight,
-    height: height || offsetWidth,
+    width,
+    height,
   };
 };
 
-export const connect = (prev, target, next) => {
-  const offsetBlock1 = getOffset(target);
-  const offsetBlock2 = getOffset(prev);
+export const connect = ({ element, targetElement }: Record<string, HTMLElement>) => {
+  const offsetBlock1 = getOffset(targetElement);
+  const offsetBlock2 = getOffset(element);
 
-  // x1, y1 - координата угла block1
+  // x1, y1 - coordinates for angle offsetBlock1
   const x1 = offsetBlock1.left + offsetBlock1.width;
   const y1 = offsetBlock1.top + offsetBlock1.height / 2;
 
-  // x2, y2 - координата угла block2
+  // x2, y2 - coordinates for angle offsetBlock2
   const x2 = offsetBlock2.right + offsetBlock2.width;
   const y2 = offsetBlock2.top + offsetBlock2.height / 2;
 
-  // x2, y2 - координата угла block2
-  // const x3 = offsetBlock3.left + offsetBlock3.width;
-  // const y3 = offsetBlock3.top;
-
-  // формулу расстояния для расчета расстояния length между двумя углами
+  // formula for getting distance between elements;
   const length = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
 
-  const cx = ((x1 + x2) / 2) - (length / 2);
-  const cy = ((y1 + y2) / 2);
+  // getting left and top offset
+  const cx = `${((x1 + x2) / 2) - (length / 2)}px`;
+  const cy = `${((y1 + y2) / 2)}px`;
 
-  const angle = Math.atan2((y1 - y2), (x1 - x2)) * (180 / Math.PI);
+  const angle = `${Math.atan2((y1 - y2), (x1 - x2)) * (180 / Math.PI)}deg`;
 
-  return `<div style='height:1px; position:absolute; left:${cx}px; top:${cy}px; width:${length}px; background: black; transform:rotate(${angle}deg);' />`;
+  return `<div style="height: 1px;position:absolute;left:${cx};top:${cy};width:${length}px;background: #000;rotate:${angle};" />`;
 };
