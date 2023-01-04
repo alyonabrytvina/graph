54f6e4259bd3dfa1327601016b8615f5af2de778
Alyona <alyona.ivanova.kh@gmail.com>
2023-01-04 20:11:25 +0100

    create interfaces

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,37 +1,63 @@
-export interface SourceChild {
-    sourceName: string;
-}
-
-export interface TargetChild {
-    targetName: string;
-}
-
-export interface Source {
+export interface SourceItem {
     id: string;
-    name: string;
-    key?: string;
+    name?: string;
 }
 
-export interface ActionRoot {
+export interface ActionItem {
     id: string;
-    prev: string[];
-    next: string;
-    actionIndex?: number;
+    prev?: string[];
+    next?: string;
 }
 
-export interface Target {
+export interface TargetItem {
     id: string;
     name: string;
 }
 
 export interface Transformation {
-    sources: Source[];
-    actions: ActionRoot[];
-    target: Target;
+    sources: SourceItem[];
+    actions: ActionItem[];
+    target: TargetItem;
 }
 
 export interface Mapping {
-    sources: Source[];
-    actions: ActionRoot[];
-    targets: Target[];
+    sources: SourceItem[];
+    actions: ActionItem[];
+    targets: TargetItem[];
+}
+
+export interface NodesList {
+    isSelected: boolean;
+    nodeIndex: number;
+}
+
+export interface Tree {
+    id: string;
+    name?: string;
+    prev?: string[];
+    next?: string;
+}
+
+export interface DepthFirstSearch {
+    graph: HTMLCollection;
+    rootNode: TargetItem;
+    tree: Tree[];
+}
+
+export interface TargetRoot {
+    target: TargetItem
+    sources: SourceItem[]
+    actions: ActionItem[]
+    currentTransformation: Transformation[]
+    graphRef: HTMLDivElement
+}
+
+export interface SelectedNode {
+    graph: HTMLDivElement;
+    isSelected: boolean
+}
+
+export interface InitialValue {
+    windowWidth: number
+    setWindowWidth: (width: number) => void
 }
