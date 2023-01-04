9b38f1996b1ae49abcf83611a459731884bc029e
Alyona <alyona.ivanova.kh@gmail.com>
2023-01-04 20:20:33 +0100

    implement logic for component

--- src/components/Target/Target.tsx
+++ src/components/Target/Target.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import "./Target.scss";
-import { TargetChild } from "../../interfaces/interfaces";
+import { depthFirstSearch } from "../../utils/depthFirstSearch";
+import { TargetRoot } from "../../interfaces/interfaces";
+import { Context } from "../../context/StateWidthProvider";
 
-export const Target = ({ targetName }: TargetChild) => (
-  <div className={targetName}>
-    {targetName}
-  </div>
-);
+export const Target = ({
+  graphRef, target, sources, actions, currentTransformation,
+}: TargetRoot) => {
+  const tree = [...actions, ...sources, target]; // convert to single array with objects
+  const value = useContext(Context);
+  const { windowWidth } = value;
+
+  useEffect(() => {
+    if (graphRef) {
+      return [...graphRef.children].forEach(({ children }) => {
+        depthFirstSearch({
+          graph: children, rootNode: target, tree,
+        });
+      });
+    }
+  }, [windowWidth, currentTransformation]);
+
+  return (
+    <div className={target.id}>
+      {target.id}
+    </div>
+  );
+};
