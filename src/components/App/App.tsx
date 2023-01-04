4c4957e1e01d9b2afb99562073dfde0f66429a11
Alyona <alyona.ivanova.kh@gmail.com>
2023-01-04 20:17:46 +0100

    implement logic for component, add textarea

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,40 +1,140 @@
-import React from "react";
+import React, {
+  useContext, useEffect, useRef, useState,
+} from "react";
 import "./App.scss";
 import { Source } from "../Source/Source";
 import { Action } from "../Action/Action";
-import { transformations } from "../../api/transformations";
 import { Target } from "../Target/Target";
-import { ActionRoot } from "../../interfaces/interfaces";
+import {
+  ActionItem, NodesList, SourceItem, Transformation,
+} from "../../interfaces/interfaces";
+import {
+  cleanNodeLines,
+  getCurrentTransformation,
+} from "../../utils/getTransformation";
+import { cleanNodes, getSelectedNode } from "../../utils/getSelectedNode";
+import { hasJsonStructure } from "../../utils/hasJsonStructure";
+import { Context } from "../../context/StateWidthProvider";
 
-export const App = () => (
-  <>
-    {transformations.map(({ sources, actions, target }, index: number) => {
-      const { id: targetId } = target;
+export const App = () => {
+  const graphRef = useRef<HTMLDivElement>(null);
 
-      return (
-        <div className="graph">
-          <div className="graph__sources">
-            {sources.map(({ id }: Record<string, string>) => (
-              <Source
-                key={id}
-                sourceName={id}
-              />
-            ))}
-          </div>
-          <div className="graph__actions">
-            {actions.map((props: ActionRoot) => (
-              <Action
-                key={props.id}
-                props={props}
-                actionIndex={index}
-              />
-            ))}
-          </div>
-          <div className="graph__targets">
-            <Target targetName={targetId} />
-          </div>
-        </div>
-      );
-    })}
-  </>
-);
+  const [nodeList, setNodeList] = useState<NodesList[]>([]);
+  const [currentTransformation, setCurrentTransformation] = useState<Transformation[]>([]);
+
+  const [currentValue, setCurrentValue] = useState<string>("");
+  const [isError, setIsError] = useState<boolean>(false);
+  const value = useContext(Context);
+
+  useEffect(() => {
+    if (currentValue.length) {
+      cleanNodeLines(graphRef.current!);
+
+      const isJson = hasJsonStructure(currentValue);
+      setIsError(!isJson);
+      const parsedValue = isJson && JSON.parse(currentValue);
+      if (typeof parsedValue !== "object") {
+        setCurrentTransformation([]);
+        return;
+      }
+
+      const {
+        transformation,
+        nodesList,
+      }: { transformation: Transformation[]; nodesList: NodesList[] } = getCurrentTransformation(parsedValue)!;
+
+      setCurrentTransformation(transformation);
+      setNodeList(nodesList);
+    } else {
+      setCurrentValue("");
+      setCurrentTransformation([]);
+    }
+  }, [currentValue, value?.windowWidth]);
+
+  const selectNode = (event: React.MouseEvent<HTMLElement>, index: number) => {
+    const parent = ((event.target as HTMLElement).parentNode as HTMLDivElement).parentNode! as HTMLDivElement;
+
+    if (parent.className === "graph") { // check for possibilities to select elements only inside graph
+      const updatedList:NodesList[] = nodeList.map((node) => {
+        const { isSelected, nodeIndex } = node;
+
+        return (index === nodeIndex) ? {
+          isSelected: !isSelected,
+          nodeIndex: index,
+        } : node;
+      });
+      setNodeList(updatedList);
+
+      updatedList.forEach(({ isSelected, nodeIndex }) => {
+        if (index === nodeIndex) {
+          return getSelectedNode({ graph: parent, isSelected });
+        }
+      });
+    }
+  };
+
+  const updateCurrentValue = (event:React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (currentValue !== event.target.value) {
+      cleanNodes(graphRef.current!);
+      setCurrentValue(event.target.value);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <span>Please, enter a valid JSON. For converting Object to JSON you can use the </span>
+        <a
+          href="https://www.convertsimple.com/convert-javascript-to-json/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          link
+        </a>
+      </div>
+      <textarea
+        style={{
+          borderColor: `${isError ? "red" : "black"}`,
+        }}
+        onChange={updateCurrentValue}
+      />
+      <div ref={graphRef}>
+        {currentTransformation?.map((props, index) => {
+          const { sources, actions, target } = props;
+          return (
+            <div
+              className="graph"
+              onClick={(event : React.MouseEvent<HTMLElement>) => selectNode(event, index)}
+            >
+              <div className="graph__sources">
+                {sources.map(({ id }: SourceItem) => (
+                  <Source
+                    key={id}
+                    id={id}
+                  />
+                ))}
+              </div>
+              <div className="graph__actions">
+                {actions.map(({ id }: ActionItem) => (
+                  <Action
+                    key={id}
+                    id={id}
+                  />
+                ))}
+              </div>
+              <div className="graph__targets">
+                <Target
+                  graphRef={graphRef.current!}
+                  target={target}
+                  sources={sources}
+                  actions={actions}
+                  currentTransformation={currentTransformation}
+                />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
