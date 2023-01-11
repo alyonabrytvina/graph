196470dd0cbe45e4072dfc93beff4a2e530895d3
Alyona <alyona.ivanova.kh@gmail.com>
2023-01-11 09:53:05 +0100

    transfer logic for parent component, make target stupid

--- src/components/Target/Target.tsx
+++ src/components/Target/Target.tsx
@@ -1,36 +1,9 @@
-import React, { useContext, useEffect } from "react";
+import React from "react";
 import "./Target.scss";
-import { depthFirstSearch } from "../../utils/depthFirstSearch";
-import { TargetRoot } from "../../interfaces/interfaces";
-import { Context } from "../../context/StateWidthProvider";
-import { getDeepestChildren } from "../../utils/getTransformation";
+import { TargetParams } from "../../interfaces/interfaces";
 
-export const Target = ({
-  graphRef, target, sources, actions, currentTransformation,
-}: TargetRoot) => {
-  const tree = [...actions, ...sources, target]; // convert to single array with objects
-  const value = useContext(Context);
-  const { windowWidth } = value;
-
-  useEffect(() => {
-    if (graphRef) {
-      tree.forEach((treeNodes) => {
-        if ("prev" in treeNodes) {
-          getDeepestChildren(graphRef, treeNodes);
-        }
-      });
-
-      return [...graphRef.children].forEach(({ children }) => {
-        depthFirstSearch({
-          graph: children, rootNode: target, tree,
-        });
-      });
-    }
-  }, [windowWidth, currentTransformation]);
-
-  return (
-    <div className={target.id}>
-      {target.id}
-    </div>
-  );
-};
+export const Target = ({ id }: TargetParams) => (
+  <div className={id}>
+    {id}
+  </div>
+);
